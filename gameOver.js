45a1e3bf60b30103f215316357dc55cb7b92f8d2
VLixed <100478256+VLixed@users.noreply.github.com>
2023-06-01 12:10:10 +0100

    Add files via upload

--- gameOver.js
+++ gameOver.js
@@ -4,6 +4,8 @@ const username = urlParams.get("username");
 const causeText = document.querySelector("#causeText");
 const tryAgainText = document.querySelector("#tryAgainText");
 const defeatSfx = document.querySelector("#defeat");
+const victorySfx = document.querySelector("#victory");
+const image = document.querySelector("img");
 
 if (username != "") {
   tryAgainText.textContent = "Try again, " + username + "?";
@@ -12,30 +14,38 @@ switch (cause) {
   case "chica":
     causeText.textContent = "When you see her, shock her";
     defeatSfx.play();
+    image.src = "images/chica.jpg";
     break;
   case "bonnie":
     causeText.textContent = "Next time do yourself a favor and shock him";
     defeatSfx.play();
+    image.src = "images/bonnie.jpg";
     break;
   case "bonnieShock":
     causeText.textContent = "He will get you if you shock at the wrong time.";
     defeatSfx.play();
+    image.src = "images/bonnie.jpg";
     break;
   case "chicaShock":
     causeText.textContent = "I don't think you should shock for no reason, no?";
     defeatSfx.play();
+    image.src = "images/chica.jpg";
     break;
   case "chicaTime":
     causeText.textContent =
       "Should have paid some attention to the right hallway!";
     defeatSfx.play();
+    image.src = "images/chica.jpg";
     break;
   case "bonnieTime":
     causeText.textContent = "Keep a look at the left hallway next time.";
     defeatSfx.play();
+    image.src = "images/bonnie.jpg";
     break;
   case "win":
     causeText.textContent = "Congrats! you survived till 6 AM.";
+    victorySfx.play();
+    image.src = "images/ballons.jpg";
     break;
 }
 
