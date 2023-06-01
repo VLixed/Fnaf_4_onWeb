7a0bcdd3512a8efc9cada9902108a5288eb52b65
VLixed <100478256+VLixed@users.noreply.github.com>
2023-06-01 09:30:59 +0100

    Delete Fnaf directory

--- Fnaf/start.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("startButton").addEventListener("click", function () {
-  const username = document.getElementById("username").value;
-  window.location.href = "game.html?username=" + encodeURIComponent(username);
-});
